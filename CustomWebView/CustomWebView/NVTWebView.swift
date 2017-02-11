93f874708c359e766e854165c45bdbc8fd9d6b7a
Tuuu <tu@techmaster.vn>
2017-02-11 10:20:43 +0700

    Revert "//page control"
    This reverts commit 7bb6b53687a6d8d98100318ad3f7e79fc6043e25.

--- CustomWebView/CustomWebView/NVTWebView.swift
+++ CustomWebView/CustomWebView/NVTWebView.swift
@@ -9,125 +9,46 @@
 import UIKit
 
 class NVTWebView: UIViewController{
-    
-    var toolBar: BaseToolBar!
-    var toolBarDetail: ToolBarDetail!
-    var myPageScrollView: MyPagedFlowView!
-    var webviewModel: NVTWebViewModel!
+    var myPageScrollView: HGPageScrollView!
+    var webviewModel = NVTWebViewModel()
     let pageId = "pageId"
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        let sizeView = self.view.frame.size
-        let centerView = self.view.center
-        let frameToShow = CGRect(x: centerView.x - sizeView.width/2, y: centerView.y - sizeView.height/2, width: sizeView.width/2, height: sizeView.height/2)
-        webviewModel = NVTWebViewModel(frame:frameToShow)
-        addToolBar()
         addPageScrollView()
-    }
-    func addToolBar()
-    {
-        if (self.toolBar == nil)
-        {
-            addToolBarNewPage()
-        }
-        else
-        {
-            self.toolBar.removeFromSuperview()
-            if (self.toolBar.isToolBarNewPage == false)
-            {
-                addToolBarNewPage()
-            }
-            else
-            {
-                addToolBarDetail()
-            }
-        }
-        self.toolBar.delegate = self
-        self.view.addSubview(self.toolBar)
-    }
-    func addToolBarNewPage()
-    {
-        self.toolBar = ToolBarNewPage(frame: CGRect(x: 0, y: self.view.frame.size.height-40, width: self.view.frame.size.width, height: 40))
-    }
-    func addToolBarDetail()
-    {
-        self.toolBar = ToolBarDetail(frame: CGRect(x: 0, y: self.view.frame.size.height-40, width: self.view.frame.size.width, height: 40))
     }
     func addPageScrollView()
     {
-        self.myPageScrollView = MyPagedFlowView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height-40))
-        self.myPageScrollView.scrollView.delegate = self
-        self.myPageScrollView.scrollView.dataSource = self
+        myPageScrollView = Bundle.main.loadNibNamed("HGPageScrollView", owner: self, options: nil)?.first as! HGPageScrollView!
         self.view.addSubview(myPageScrollView)
     }
-    
-    func layoutPageScrollView()
-    {
-        myPageScrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let horizontalConstraint = NSLayoutConstraint(item: myPageScrollView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 0)
-        let verticalConstraint = NSLayoutConstraint(item: myPageScrollView, attribute: .left, relatedBy: .equal, toItem: self.view, attribute: .left, multiplier: 1, constant: 0)
-        let widthConstraint = NSLayoutConstraint(item: myPageScrollView, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: .right, multiplier: 1, constant: 0)
-        let heightConstraint = NSLayoutConstraint(item: myPageScrollView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: -40)
-        
-        view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
-    }
 }
-extension NVTWebView: ToolBarDelegate
+extension NVTWebView: HGPageScrollViewDataSource
 {
-    func addNewPage()
-    {
+    func numberOfPages(in scrollView: HGPageScrollView!) -> Int {
+        return webviewModel.myPageDataArray.count
+    }
+    func pageScrollView(_ scrollView: HGPageScrollView!, viewForPageAt index: Int) -> HGPageView! {
+        var currentWebView = webviewModel.myPageDataArray[index] as! CustomWebView
+        if let customWebView = scrollView.dequeueReusablePage(withIdentifier: pageId) as? CustomWebView
+        {
+            currentWebView = customWebView
+        }
+        currentWebView.loadRequest()
+        currentWebView = webviewModel.setupDataToView(currentView: currentWebView)
+        currentWebView.frame.size.height = 420
+        return currentWebView
         
-
     }
-
-    func showCurrentPage()
-    {
-//        showPage(with: self.myPageScrollView, at: -1)
-        addToolBar()
+    func pageScrollView(_ scrollView: HGPageScrollView!, titleForPageAt index: Int) -> String! {
+        return "title"
     }
-    func showPreviousPage()
-    {}
-    func showForwardPage()
-    {}
-    func actionBookMark()
-    {}
-    func showBrowser()
-    {
-        showCurrentPage()
-    }
-    func showDownloadView()
-    {}
-    func zoom()
-    {}
-    func switchToolBar()
-    {
-        
+    func pageScrollView(_ scrollView: HGPageScrollView!, subtitleForPageAt index: Int) -> String! {
+        return "subTitle"
     }
 }
-extension NVTWebView: PagedFlowViewDataSource
+extension NVTWebView: HGPageScrollViewDelegate
 {
-    func numberOfPages(in flowView: PagedFlowView!) -> Int {
-        return webviewModel.myPageDataArray.count;
-    }
-    func flowView(_ flowView: PagedFlowView!, cellForPageAt index: Int) -> UIView! {
-//        var currentView = flowView.dequeueReusableCell()
-//        if(currentView == nil)
-//        {
-//            currentView = webviewModel.myPageDataArray[index]
-//        }
-        return webviewModel.myPageDataArray.object(at: index) as! UIView
+    func pageScrollView(_ scrollView: HGPageScrollView!, didDeselectPageAt index: Int) {
         
     }
 }
-extension NVTWebView: PagedFlowViewDelegate
-{
-    func sizeForPage(in flowView: PagedFlowView!) -> CGSize {
-        return CGSize(width: 200, height: 400)
-    }
-}
-
