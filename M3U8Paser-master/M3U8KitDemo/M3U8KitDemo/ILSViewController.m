be7d1581e5fc6e682e93796731ec9667f883e4e9
Tuuu <tu@techmaster.vn>
2017-03-14 18:07:03 +0700

    demo

--- M3U8Paser-master/M3U8KitDemo/M3U8KitDemo/ILSViewController.m
+++ M3U8Paser-master/M3U8KitDemo/M3U8KitDemo/ILSViewController.m
@@ -30,7 +30,7 @@
         
         NSTimeInterval begin = [NSDate timeIntervalSinceReferenceDate];
         
-        NSString *baseURL = @"http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/";
+        NSString *baseURL = @"http://qthttp.apple.com.edgesuite.net/";
         NSString *path = [[NSBundle mainBundle] pathForResource:@"sl" ofType:@"m3u8"];
         NSError *error;
         NSString *str = [[NSString alloc] initWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
@@ -39,7 +39,7 @@
         }
         
         M3U8PlaylistModel *medel = [[M3U8PlaylistModel alloc] initWithString:str baseURL:baseURL error:NULL];
-        
+    
         NSLog(@"segments names: %@", [medel segmentNamesForPlaylist:medel.mainMediaPl]);
         
         NSString *m3u8Path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"str.m3u8"];
@@ -54,7 +54,58 @@
         
     });
 }
-
+- (void) parseM3u8
+{
+    NSString *plainString = [self.url m3u8PlanString];
+    BOOL isMasterPlaylist = [plainString isMasterPlaylist];
+    
+    
+    NSError *error;
+    NSURL *baseURL;
+    
+    if(isMasterPlaylist)
+    {
+        
+        M3U8MasterPlaylist *masterList = [[M3U8MasterPlaylist alloc] initWithContentOfURL:self.url error:&error];
+        self.masterPlaylist = masterList;
+        
+        M3U8ExtXStreamInfList *xStreamInfList = masterList.xStreamList;
+        M3U8ExtXStreamInf *StreamInfo = [xStreamInfList extXStreamInfAtIndex:0];
+        
+        NSString *URI = StreamInfo.URI;
+        NSRange range = [URI rangeOfString:@"dailymotion.com"];
+        NSString *baseURLString = [URI substringToIndex:(range.location+range.length)];
+        baseURL = [NSURL URLWithString:baseURLString];
+        
+        plainString = [[NSURL URLWithString:URI] m3u8PlanString];
+    }
+    
+    
+    
+    M3U8MediaPlaylist *mediaPlaylist = [[M3U8MediaPlaylist alloc] initWithContent:plainString baseURL:baseURL];
+    self.mediaPlaylist = mediaPlaylist;
+    
+    M3U8SegmentInfoList *segmentInfoList = mediaPlaylist.segmentList;
+    
+    NSMutableArray *segmentUrls = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < segmentInfoList.count; i++)
+    {
+        M3U8SegmentInfo *segmentInfo = [segmentInfoList segmentInfoAtIndex:i];
+        
+        NSString *segmentURI = segmentInfo.URI;
+        NSURL *mediaURL = [baseURL URLByAppendingPathComponent:segmentURI];
+        
+        [segmentUrls addObject:mediaURL];
+        if(!self.segmentDuration)
+            self.segmentDuration = segmentInfo.duration;
+    }
+    
+    self.segmentFilesURLs = segmentUrls;
+    
+    
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
--- PlayU3M8/PlayU3M8/ViewController.swift
+++ PlayU3M8/PlayU3M8/ViewController.swift
@@ -8,32 +8,53 @@
 
 import UIKit
 import MediaPlayer
-class ViewController: UIViewController {
+import AVKit
 
-    var moviePlayer : MPMoviePlayerController?
+class ViewController: UIViewController {
+    
+    var moviePlayer : AVPlayerViewController?
     var videoPlayer:AVPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         playVideo()
         let fileURL = NSURL(string: "")
-
+        
+    }
+    
+    @IBAction func action(_ sender: Any) {
+        print(getURL())
+    }
+    func getURL() -> URL?
+    {
+        let asset = videoPlayer.currentItem?.asset
+        if asset == nil {
+            return nil
+        }
+        if let urlAsset = asset as? AVURLAsset {
+            return urlAsset.url
+        }
+        return nil
     }
     private func playVideo() {
-        let path = "http://74.208.128.124:1935/live/myStream/playlist.m3u8"
- 
-            //path = NSBundle.mainBundle().pathForResource("movie", ofType:"m4v"),
-            
-            let url = NSURL(string: path)
-            let moviePlayer = MPMoviePlayerController(contentURL: url as URL!)
-            self.moviePlayer = moviePlayer
-            moviePlayer?.view.frame = self.view.bounds
-            moviePlayer?.prepareToPlay()
-            moviePlayer?.scalingMode = .aspectFill
-            self.view.addSubview((moviePlayer?.view)!)
-
+        let path = "http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8"
+        
+        //path = NSBundle.mainBundle().pathForResource("movie", ofType:"m4v"),
+        
+        moviePlayer = AVPlayerViewController()
+        
+        let url = NSURL(string: path)
+        self.videoPlayer = AVPlayer(url: url! as URL)
+        self.moviePlayer?.player = videoPlayer
+        self.addChildViewController(moviePlayer!)
+        self.view.addSubview((moviePlayer?.view)!)
+        
+        
+        moviePlayer?.view.frame = CGRect(x: 0, y: 50, width: self.view.frame.width, height: self.view.frame.height)
+        
+        
     }
-
-
+    
+    
 }
 
